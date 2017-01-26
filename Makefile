91b5fceb896ff1eeb3622504a1cb42e4f118e30d
Arnaud Pflieger <pflieger.arnaud@gmail.com>
2017-01-27 00:46:21 +0100

    make cover

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 fmt:
 	go fmt ./...
+
+cover:
+	./code-coverage.sh
+	go tool cover -html=coverage.txt
--- code-coverage.sh
+++ code-coverage.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 set -e
-echo "" > coverage.txt
+echo "mode: set" > coverage.txt
 
 for d in $(go list ./... | grep -v vendor); do
-    go test -race -coverprofile=profile.out -covermode=atomic $d
+    go test -coverprofile=profile.out $d
     if [ -f profile.out ]; then
-        cat profile.out >> coverage.txt
+        cat profile.out | grep -v "mode: set" >> coverage.txt
         rm profile.out
     fi
 done
